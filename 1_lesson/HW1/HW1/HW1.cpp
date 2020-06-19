66748d2be421eb0e289edfa859a1fd44f8dc257a
mike <mykhail.nakonechnyi@gmail.com>
2020-06-19 03:40:30 -0500

    homework 1

---
+++ 1_lesson/HW1/HW1/HW1.cpp
@@ -0,0 +1,42 @@
+// HW1.cpp : The MIT License.
+// source: https://opensource.org/licenses/MIT
+// understanding: https://habr.com/ru/post/310976/
+// author: Nakonechnyi Mikhail
+
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleOutputCP(1252);
+    // (char)169 == © // 0xA9
+    std::cout <<    "Copyright " << (char)169 << " <2020> <Nakonechnyi Mikhail>\n"
+                    "\nPermission is hereby granted, free of charge, to any person obtaining a copy of this softwareand associated documentation files(the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and /or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions :\n"
+                    "\nThe above copyright noticeand this permission notice shall be included in all copies or substantial portions of the Software.\n"
+                    "\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
